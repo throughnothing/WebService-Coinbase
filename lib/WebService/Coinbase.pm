a098b1383f9702d0938c4f5680fda6e2a76850d1
William Wolf <throughnothing@gmail.com>
2014-08-30 01:39:15 -0700

    Initial work + create_button call

--- lib/WebService/Coinbase.pm
+++ lib/WebService/Coinbase.pm
@@ -1,7 +1,35 @@
 use strict;
 use warnings;
 package WebService::Coinbase;
+use Moo;
+with 'WebService::BaseClientRole';
 
-# ABSTRACT:
+# ABSTRACT: Simple client for Coinbase API
+
+use Crypt::Mac::HMAC qw(hmac_hex);
+use Time::HiRes qw(time);
+
+has api_key     => ( is => 'ro', required => 1 );
+has api_secret  => ( is => 'ro', required => 1 );
+has '+base_url' => ( default => 'https://coinbase.com/api/v1' );
+
+# Calculate the NONCE + ACCESS_SIGNATURE headers for AUTH
+around _req => sub {
+    my ($orig, $self, $req) = (shift, shift, shift);
+    my $nonce = time * 1E5;
+    my $sig   = hmac_hex('SHA256', $self->api_secret,
+        $nonce, $req->uri, $req->content);
+    $req->header(
+        ACCESS_KEY => $self->api_key,
+        ACCESS_NONCE => $nonce,
+        ACCESS_SIGNATURE => $sig,
+    );
+    return $self->$orig($req, @_);
+};
+
+sub create_button {
+    my ($self, $data) = @_;
+    return $self->post("/buttons", { button => $data });
+}
 
 1;
